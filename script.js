80cf97c870d688169e2bdd7ea7f11bbf9cc42284
Pre2010 <preyesh2010@yahoo.com>
2020-12-19 20:46:39 -0600

    Updated the API, added an error container, retry text, and error text.

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+// counts for retrying to fetch quote
+let count = 0;
+
+// empty array to hold our quotes
+let apiQuotes = [];
+
+// holds the quote
+let quote;
+
+// random number for our quotes
+let randomQuoteNum;
+
+// setting our constants to our html elements
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+const errorContainer = document.getElementById('error-container');
+const errorText = document.getElementById('error-text');
+const retryText = document.getElementById('retry-text');
+
+function showErrorContainer() {
+    retryText.hidden = false;
+    errorText.hidden = true;
+    errorContainer.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeErrorContainer() {
+    errorContainer.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+function removeRetryTextShowErrorText() {
+    retryText.hidden = true;
+    errorText.hidden = false;
+}
 
 function showLoadingSpinner() {
     loader.hidden = false;
@@ -17,48 +50,6 @@ function removeLoadingSpinner() {
     }
 }
 
-async function getQuote() {
-    showLoadingSpinner();
-
-    // to avoid the cors error by using a proxy url.
-    const proxyUrl ='https://cors-anywhere.herokuapp.com/';
-    // https://forismatic.com/en/api/
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-
-    try {
-        // https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
-        // we call the proxy url first which adds the cors header to our request, and then the api url
-        // waits until the fetch request completes for the proxyUrl and apiUrl
-        const response = await fetch(proxyUrl + apiUrl);
-        
-        // waits for the response in json format
-        const data = await response.json();
-        
-        // if author is blank, put Unknown.
-        // else, element with author
-        if (data.quoteAuthor === '') {
-            authorText.innerText = 'Unknown';
-        } else {
-            authorText.innerText = data.quoteAuthor;
-        }
-
-        // reduce font size for long quotes
-        if (data.quoteText.length > 50) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote');
-        }
-        quoteText.innerText = data.quoteText;
-
-        // stop loader and show quote
-        removeLoadingSpinner();
-    } catch (error) {
-        // getQuote();
-        
-    }
-}
-
-// tweet quote
 function tweetQuote() {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
@@ -67,9 +58,64 @@ function tweetQuote() {
     window.open(twitterUrl, '_blank');
 }
 
+// sets a random number to choose which quote to display.
+// and sets it to the constant quote
+function getQuote() {
+    quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
 //  event listeners for buttons
-newQuoteBtn.addEventListener('click', getQuote);
+newQuoteBtn.addEventListener('click', getAndSetQuotes);
 twitterBtn.addEventListener('click', tweetQuote);
 
+
+async function getAndSetQuotes() {
+    showLoadingSpinner();
+
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        // waits until the fetch request completes for the apiUrl
+        const response = await fetch(apiUrl);
+
+        // waits for the response in json format and
+        // gets the json from our response and turning it into a json object
+        apiQuotes = await response.json();
+                
+        getQuote();
+
+        // if author is blank, put Unknown.
+        if (quote.author === '' || quote.author === null) {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = quote.author;
+        }
+
+        // reduce font size for long quotes
+        if (quote.text.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = quote.text;
+
+        // stop loader and show quote
+        removeLoadingSpinner();
+        removeErrorContainer();
+        count = 0;
+    } catch (error) {
+        count++;
+        showErrorContainer();
+        if (count >= 3) {
+            removeRetryTextShowErrorText();
+            console.error("Error: ", error);
+            return;
+        } else {
+            getAndSetQuotes();
+        }
+        
+    }
+}
+
 // on page load
-getQuote();
+getAndSetQuotes();
